f93dd3fbc721299730e7bc2bf47a312ba6892ed6
Shae Hazelwood <Hazelwood@Shaes-MacBook-Pro-2.local>
2013-11-05 15:24:19 -0500

    Lab4
    finished

--- Hazelwood_Lab4/Hazelwood_Logic_Tables.js
+++ Hazelwood_Lab4/Hazelwood_Logic_Tables.js
@@ -8,12 +8,20 @@
 var p;
 var q;
 var r;
+var  a = 0;
 
-p = confirm("First value:  Click OK for true or Cancel for false.");
-q = confirm("Second value:  Click OK for true or Cancel for false.");
-r = confirm("Third value:  Click OK for true or Cancel for false.");
-if (p || (q||r)) {
-    console.log("With " + p + " or " + (q + " or " + r) + ", the outcome is TRUE.");
-} else {
-    console.log("With " + p + " or " + (q + " or " + r) + ", the outcome is FALSE.");
-};
+while (a<8)
+	{
+	p = confirm("First value:  Click OK for true or Cancel for false.");
+	q = confirm("Second value:  Click OK for true or Cancel for false.");
+	r = confirm("Third value:  Click OK for true or Cancel for false.");
+		if (p || (q||r)) 
+		{
+    		console.log("With " + p + " or " + (q + " or " + r) + ", the outcome is TRUE.");
+    	} 		
+    	else 
+    	{
+    		console.log("With " + p + " or " + (q + " or " + r) + ", the outcome is FALSE.");
+    	}
+    	a++
+	};
